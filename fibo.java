0b00fb12a7723ddfbbfa69d8095521cbc86c6974
Palakgupta26 <Palakg431@gmail.com>
2025-01-21 11:08:49 +0530

    fib code

---
+++ fibo.java
@@ -0,0 +1,18 @@
+public class fibo {
+    public static int fib(int n){
+        if( n==0){
+            return 0;
+        }
+        if( n==1){
+            return 1;
+        }
+
+        int c = fib(n-1)+fib(n-2);
+        return c;
+    }
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+        System.out.println(fib(7));
+    }
+}
